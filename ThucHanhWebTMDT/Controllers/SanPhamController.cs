075cbf698ddfe6397c8bb13fdcf707a874f747df
HIENLTH <hyhien@gmail.com>
2018-07-21 10:41:22 +0700

    First code

---
+++ ThucHanhWebTMDT/Controllers/SanPhamController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ThucHanhWebTMDT.Models;
+namespace ThucHanhWebTMDT.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        MyEStoreEntities db = new MyEStoreEntities();
+        // GET: SanPham
+        public ActionResult Index()
+        {
+            var dsHH = db.HangHoas.OrderByDescending(p => p.GiamGia).Take(12).ToList();
+            return View(dsHH);
+        }
+    }
+}
